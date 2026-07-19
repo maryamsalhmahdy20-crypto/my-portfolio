aad28a09e44377c5b3f1fdd467ba88b349337b49
Maryam Saleh <maryam.salh.mahdy.20@gmail.com >
2026-07-19 11:53:21 +0300

    add navbar and hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,7 @@
-import Hero from "@/components/Hero";
-import dynamic from 'next/dynamic';
-
-// Import About dynamically and handle modules that may not have a default export
-const About = dynamic(() => import('@/components/About').then(mod => mod.default || mod));
-
 export default function Home() {
-	return (
-		<>
-			<Hero />
-			<About />
-		</>
-	);
+  return (
+    <div style={{ color: "white", fontSize: "50px", padding: "100px" }}>
+      TEST PAGE
+    </div>
+  );
 }
